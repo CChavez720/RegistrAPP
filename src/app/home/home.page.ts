ba510bfc318eb17b1921f44cac728655015c5d8a
CChavez720 <107702096+CChavez720@users.noreply.github.com>
2024-10-03 10:30:48 -0300

    commit home
    avances en el page de home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // Declaración de propiedades de la clase
+  userName: string = 'Juan Pérez';  // Nombre de usuario
+  userImage: string = 'assets/img/user.png';  // Ruta de la imagen del usuario
 
-  constructor() {}
-
+  constructor() {
+    // El constructor está vacío porque no necesitas inicializar nada especial aquí
+  }
 }
+
