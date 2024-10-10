714266e55f976ec994f5789d9ede324836da99a8
CChavez720 <107702096+CChavez720@users.noreply.github.com>
2024-10-10 10:29:43 -0300

    primer paso conexion bd
    conexion a medias

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+  apiKey: "AIzaSyD8MFa5s-LX_YsGpPGmqjVD02OBY9c3PkI",
+  authDomain: "registerapp720.firebaseapp.com",
+  projectId: "registerapp720",
+  storageBucket: "registerapp720.appspot.com",
+  messagingSenderId: "432310884032",
+  appId: "1:432310884032:web:8b88db1ef11596035e7f25",
+  measurementId: "G-90GCN0CVQ8"
+    }
 };
 
 /*
