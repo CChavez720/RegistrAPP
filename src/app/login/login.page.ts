46dee05ab950b58bc3a41a8da094e33695241a7c
CChavez720 <107702096+CChavez720@users.noreply.github.com>
2024-10-12 00:59:29 -0300

    pinche conexion

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FirebaseService } from '../services/firebase.service';
 import { User } from 'src/models/user.model';
+import { UtilsService } from '../services/utils.service';
 
 @Component({
   selector: 'app-login',
@@ -17,14 +18,23 @@ export class LoginPage implements OnInit{
   })
 
   firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService);
 
   ngOnInit(){    
   }
 
-  submit(){
+  async submit(){
     if(this.form.valid){
+
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
       this.firebaseSvc.signIn(this.form.value as User).then(res =>{
         console.log(res);
+      }).catch(error => {
+        console.log(error)
+      }).finally(() => {
+        loading.dismiss();
       })
     }
   }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import  { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { User } from 'src/models/user.model';
+import { AngularFirestore } from '@angular/fire/compat/firestore'
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,11 @@ import { User } from 'src/models/user.model';
 export class FirebaseService {
 
   auth = inject(AngularFireAuth);
+  firestore = inject(AngularFirestore);
 
   signIn(user: User){
     return signInWithEmailAndPassword(getAuth(), user.email, user.password)
   }
+
+  
 }
